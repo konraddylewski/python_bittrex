8b6a9169c576383d2c447d2a4074f3efa3593167
konraddylewski <konrad.dylewski@gmail.com>
2020-02-11 23:55:02 +0100

    my_api_secret correction

--- price.py
+++ price.py
@@ -4,7 +4,7 @@
 
 from bittrex.bittrex import Bittrex
 
-my_bittrex = Bittrex('my_api_key', 'my_api_key')
+my_bittrex = Bittrex('my_api_key', 'my_api_secret')
 
 bsv_price = my_bittrex.get_market_history('USDT-BSV')
 print(bsv_price['result'][0]['Price'])
