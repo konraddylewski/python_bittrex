b4b4a163f9cb6e7d7e980baab6c979240c6a3415
konraddylewski <konrad.dylewski@gmail.com>
2020-02-11 23:50:56 +0100

    Check the current BSV price

---
+++ price.py
@@ -0,0 +1,10 @@
+# first pip install python-bittrex
+# How to install in Pycharm?
+# File -> Settings -> Project interpreter -> Install -> python-bittrex
+
+from bittrex.bittrex import Bittrex
+
+my_bittrex = Bittrex('my_api_key', 'my_api_key')
+
+bsv_price = my_bittrex.get_market_history('USDT-BSV')
+print(bsv_price['result'][0]['Price'])
